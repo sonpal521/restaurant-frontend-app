e5049c5d5a165f6fb52dee9761c8c457834b6cca
sohan <sonpalp345@gmail.com>
2025-03-28 16:16:49 +0530

    Fix Some Problem

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    headers: {
+      'Content-Type': 'application/javascript',
+    },
+  },
+  build: {
+    target: 'esnext', // Ensure modern ES module support
+  },
+});
